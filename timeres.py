351d02585eb8028812659fb4f680ea4d35a2fde1
Aponsot <ponsota@oregonstate.edu>
2025-07-30 11:20:19 -0500

    Update timeres.py

--- timeres.py
+++ timeres.py
@@ -3,23 +3,28 @@ import matplotlib.pyplot as plt
 import argparse
 import h5py
 
-with h5py.File("argparse.h5", "r") as f:
-    int_val = f["intensity_stack"][:]
-    tth = f["tth"][:]
-    q = f["q_values"][:] 
+def plot_integration_results(h5_file):
+    
+    # Load data from HDF5 file
+    with h5py.file(h5_file, "r") as f:
+        int_val = f["intensity_stack"][:]
+        tth = f["tth"][:]
+        q = f["q_values"][:] 
 
-    plt.figure(figsize=(10, 6))
-    plt.imshow(int_val, aspect='auto', cmap='jet', extent=(tth.min(), tth.max(), 0, int_val.shape[0]))
-    plt.colorbar(label='Intensity')
-    plt.xlabel('q (1/Å)')
-    plt.ylabel('Frame Index') 
-    plt.show()
+        plt.figure(figsize=(10, 6))
+        plt.imshow(int_val, aspect='auto', cmap='jet', extent=(tth.min(), tth.max(), 0, int_val.shape[0]))
+        plt.colorbar(label='Intensity')
+        plt.xlabel('q (1/Å)')
+        plt.ylabel('Frame Index') 
+        plt.show()
 
 
 if __name__ == "__main__":
     # Command-line argument parsing
-    parser = argparse.ArgumentParser(description="Polar integration of diffraction experiments.")
+    parser = argparse.ArgumentParser(description="Plot polar integration results from HDF5 file.")
     parser.add_argument("h5_file", type=str, help="Path to the h5 file.")
-                        
+    
 
     args = parser.parse_args() 
+
+    plot_integration_results(args.h5_file)
