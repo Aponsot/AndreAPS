099a4e7b45d9ded8e547874636759531b393c00a
Aponsot <ponsota@oregonstate.edu>
2025-08-07 14:56:08 -0500

    Update peakfit.py

--- peakfit.py
+++ peakfit.py
@@ -54,6 +54,7 @@ def peak_fit(h5, frame_number, peak_pos, window=0.1):
             c0=10, c1=0.5  # Polynomial parameters (c0: intercept, c1: slope)
         )
         result = model.fit(int_full_limited, params, x=q_limited[peak])
+        print(f"Peak {i}: Center = {result.params['center'].value}")
         peak_fit = {
         "peak_index": i,
         "fit_result": result.best_fit,  # The fitted curve
@@ -61,7 +62,7 @@ def peak_fit(h5, frame_number, peak_pos, window=0.1):
         }
         peak_fits.append(peak_fit)  # Append the dictionary to the list
         axes[0, 2].plot(q_limited, result.best_fit, label='Fitted Peak', color='orange') 
-    print(f'Peak index: {i} {result.params['center'].value}')
+   
     axes[0, 2].plot(q_limited, int_full_limited, label='Full Data', linestyle = '--', color='green')
     axes[0, 2].plot(q_limited, background_full, label='Background', linestyle = "-",color='blue') 
     axes[0, 2].plot(q_limited[peaks_full], data_bg_sub_full[peaks_full], 'x', label='Peaks')
