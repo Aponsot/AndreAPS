f71b49325775ae0cd90f85aa0e48ec526f6548d3
Aponsot <ponsota@oregonstate.edu>
2025-08-15 09:54:23 -0500

    Update timeres.py

--- timeres.py
+++ timeres.py
@@ -3,13 +3,18 @@ import argparse
 import numpy as np
 import h5py
 import matplotlib.pyplot as plt
-
+energy = 61.335  # Energy in keV
+h = 4.135667696e-18  # Planck's constant in keV·s
+c = 2.99792458e10  # Speed of light in cm/s
+wavelength = h * c / energy  # Wavelength in cm
+wavelength *= 1e8  # Convert to angstroms
 
 def graph(h5):
     with h5py.File(h5, "r") as f:
-        q = f["q"][:]                       
+                             
         int_val = f["int"][:]
         frames = int_val.shape[0]
+        q = (4*np.pi/ wavelength) * np.sin(np.deg2rad(int_val[1]/2.0))
         plt.figure(figsize=(10, 6))
         plt.imshow(int_val, aspect='auto', extent=[2, 7, 0, frames],
                    origin='lower', cmap='jet')
