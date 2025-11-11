b1b239debe88d3e0ddd6ca56cb2654e4fef9882d
Aponsot <ponsota@oregonstate.edu>
2025-11-11 14:23:25 -0600

    Update batch_analysis.py

--- batch_analysis.py
+++ batch_analysis.py
@@ -31,7 +31,7 @@ DRIFT_NEG = 0.20
 DRIFT_POS = 0.090
 
 # Minimum separation
-MIN_SEP = 0.00040
+MIN_SEP = 0.0040
 
 # Acceptance requirement (ΔAIC improvement)
 AIC_IMPROVE = 1.0
@@ -741,6 +741,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
 
 
