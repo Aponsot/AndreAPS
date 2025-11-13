6b6503308f1935861d45591e7ef0ab998f30f77d
Aponsot <ponsota@oregonstate.edu>
2025-11-13 15:46:36 -0600

    Update batch_analysis.py

--- batch_analysis.py
+++ batch_analysis.py
@@ -44,7 +44,7 @@ MAP_STEP        = 1      # step between frames (e.g., 1, 5, 10)
 
 
 SAVE_MAP_TO_H5 = True   # set True to write a peakmap HDF5
-OUTPUT_H5_PATH = r"~/Data/h5"
+OUTPUT_H5_PATH = r"~/Data/h5/220-melt.h5"
 
 
 # Pixel-integrated Gaussian
@@ -652,4 +652,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
 
